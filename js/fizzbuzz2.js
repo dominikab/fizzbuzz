08c35919d111679da5df7c6066622cace44d5267
Dominika Bienkowska <bienkowska.dominika@gmail.com>
2015-02-15 20:43:42 +0100

    first commit

---
+++ js/fizzbuzz2.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+function fizzExecutor(userInput){
+ for (var i=userInput;i<100;i++){
+    var outcome='';
+    if (i%3===0) {outcome+='Fizz ';}
+     if (i%5===0) {outcome+='Buzz';}
+    else if (i%3!==0 && i%5!==0) {outcome=i;}
+   cprint(outcome);
+ }
+
+        }
+  
+function askForNumber(){
+var input=Number(prompt("Provide a number between 1 and 100")); 
+if (input!==input) {alert("this is a string! Provide a number between 1 and 100");
+askForNumber();
+}
+else if (input%1!=0){alert("Provide a number between 1 and 100, make sure it is an integer");
+askForNumber();}
+else {fizzExecutor(input);}
+}
+
+
+function Printer() {
+ var lineCount = 1;
+
+  this.printLine = function(m) {
+    var message = lineCount + ': ' + m;
+
+    var n = document.createElement("pre"),    // create a new 'pre' element
+        t = document.createTextNode(message); // create a text node to hold our message
+
+    n.appendChild(t); // append your text to the pre element
+    document.getElementById("console").appendChild(n); // append your element to the #console element
+
+    lineCount++;
+  }
+}
+
+// create both printers
+var consolePrinter = new Printer("console");
+var cprint = consolePrinter.printLine;
+
+askForNumber();
+
+});
