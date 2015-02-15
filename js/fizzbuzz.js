40c01329dfeeb7f147ab8b1c50fde2e7b5665654
Dominika Bienkowska <bienkowska.dominika@gmail.com>
2015-02-15 15:21:59 +0100

    first commit

---
+++ js/fizzbuzz.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+for (var i=1;i<100;i++){
+
+    var outcome='';
+    if (i%3===0) {outcome+='Fizz ';}
+     if (i%5===0) {outcome+='Buzz';}
+    else if (i%3!==0 && i%5!==0) {outcome=i;}
+    $(".number").before("The number is: " +outcome+ "<br>");
+//console.log("The outcome for : "+i+" is : "+outcome);
+}
+
+
+});
